9c0f19e503b6225b7b6178fceb8ad15bab0b28ec
Alex <alexkrieg@gmx.ch>
2025-01-10 20:28:13 +0100

    ~Server start event replaced

--- neoforge/src/main/java/net/kroia/stockmarket/neoforge/NeoForgeServerEvents.java
+++ neoforge/src/main/java/net/kroia/stockmarket/neoforge/NeoForgeServerEvents.java
@@ -1,5 +1,7 @@
 package net.kroia.stockmarket.neoforge;
 
+import dev.architectury.event.events.common.LifecycleEvent;
+import net.kroia.modutilities.ModUtilitiesMod;
 import net.kroia.stockmarket.util.StockMarketServerEvents;
 import net.minecraft.server.level.ServerLevel;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -8,24 +10,18 @@ import net.neoforged.neoforge.event.level.LevelEvent;
 
 @EventBusSubscriber
 public class NeoForgeServerEvents {
-    @SubscribeEvent
-    public static void onServerStart(LevelEvent.Load event) {
-        if (event.getLevel() instanceof ServerLevel serverLevel) {
-            // Check if the world is the overworld
-            if (serverLevel.dimension().equals(ServerLevel.OVERWORLD))
-                StockMarketServerEvents.onServerStart(serverLevel.getServer());
 
-        }
+    public static void init()
+    {
+        LifecycleEvent.SERVER_STARTED.register(server -> {
+            ModUtilitiesMod.LOGGER.info("[NeoForgeSetup] SERVER_STARTING");
+            StockMarketServerEvents.onServerStart(server);
+        });
+        LifecycleEvent.SERVER_STARTING.register(server -> {
+            ModUtilitiesMod.LOGGER.info("[NeoForgeSetup] SERVER_STOPPED");
+            StockMarketServerEvents.onServerStop(server);
+        });
     }
-
-    @SubscribeEvent
-    public static void onServerStop(LevelEvent.Unload event) {
-        if (event.getLevel() instanceof ServerLevel serverLevel) {
-            if (serverLevel.dimension().equals(ServerLevel.OVERWORLD))
-                StockMarketServerEvents.onServerStop(serverLevel.getServer());
-        }
-    }
-
     @SubscribeEvent
     public static void onWorldSave(LevelEvent.Save event) {
         if (event.getLevel() instanceof ServerLevel serverLevel) {
