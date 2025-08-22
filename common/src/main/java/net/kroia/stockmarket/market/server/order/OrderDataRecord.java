16186a023d741f054c1fa4c20291df4ad04c0f16
Alex <alexkrieg@gmx.ch>
2025-08-22 16:32:55 +0200

    ~ optimizing the OrderHistoryDataArchiveChunk save/load
    ~ preventing the StockMarketDataManager from triggering a asnyc save event twice

--- common/src/main/java/net/kroia/stockmarket/market/server/order/OrderDataRecord.java
+++ common/src/main/java/net/kroia/stockmarket/market/server/order/OrderDataRecord.java
@@ -2,26 +2,56 @@ package net.kroia.stockmarket.market.server.order;
 
 import net.kroia.modutilities.networking.INetworkPayloadConverter;
 import net.kroia.modutilities.persistence.ServerSaveable;
+import net.kroia.stockmarket.market.TradingPair;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.FriendlyByteBuf;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.UUID;
 
 public class OrderDataRecord implements ServerSaveable, INetworkPayloadConverter{
+
+    private final static UUID TEST_DUMMY_UUID = UUID.randomUUID(); // For testing purposes, replace with a valid UUID if needed
+    public static final class KEY
+    {
+        public static final String AMOUNT = "a";
+        public static final String PLAYER = "p";
+        public static final String ENUM_DATA = "e";
+        public static final String TRADING_PAIR = "t";
+    }
 
     long amount;
     UUID player;
     Order.Type type;
     Order.Status status;
 
-    public OrderDataRecord(long amount, UUID player, Order.Type type, Order.Status status){
+
+    // The trading pair is not saved inside the save/load methods!
+    TradingPair tradingPair;
+
+    public OrderDataRecord(long amount, @NotNull UUID player, Order.Type type, Order.Status status, TradingPair tradingPair) {
         this.amount = amount;
         this.player = player;
         this.type = type;
         this.status = status;
+        this.tradingPair = tradingPair;
     }
 
-    public OrderDataRecord(){}
+    private OrderDataRecord(){}
+    public static OrderDataRecord loadFromTag(CompoundTag tag, TradingPair pair)
+    {
+        OrderDataRecord record = new OrderDataRecord();
+        record.tradingPair = pair;
+        if(!record.load(tag)){
+            return null; // Failed to load
+        }
+        return record;
+    }
+
+    public TradingPair getTradingPair() {
+        return tradingPair;
+    }
 
     @Override
     public void decode(FriendlyByteBuf friendlyByteBuf) {
@@ -42,27 +72,34 @@ public class OrderDataRecord implements ServerSaveable, INetworkPayloadConverter
 
     @Override
     public boolean save(CompoundTag compoundTag) {
-        compoundTag.putLong("amount", amount);
-        compoundTag.putUUID("player", player != null ? player : UUID.randomUUID());
-        compoundTag.putByte("enumData", (byte)((type.ordinal() << 4) | status.ordinal()));
+        compoundTag.putLong(KEY.AMOUNT, amount);
+        compoundTag.putUUID(KEY.PLAYER, player);
+        compoundTag.putByte(KEY.ENUM_DATA, (byte)((type.ordinal() << 4) | status.ordinal()));
         return true;
     }
 
     @Override
     public boolean load(CompoundTag compoundTag) {
-        if(!compoundTag.contains("amount") || !compoundTag.contains("player") || !compoundTag.contains("enumData")){
+        if(!compoundTag.contains(KEY.AMOUNT) || !compoundTag.contains(KEY.PLAYER) || !compoundTag.contains(KEY.ENUM_DATA)){
             return false;
         }
-        amount = compoundTag.getLong("amount");
-        player = compoundTag.getUUID("player");
-        byte enumData = compoundTag.getByte("enumData");
+        amount = compoundTag.getLong(KEY.AMOUNT);
+        player = compoundTag.getUUID(KEY.PLAYER);
+        byte enumData = compoundTag.getByte(KEY.ENUM_DATA);
         type = Order.Type.values()[enumData >> 4 & 0x0F];
         status = Order.Status.values()[enumData & 0x0F];
 
         return true;
     }
 
-    public static OrderDataRecord fromOrder(Order order){
-        return new OrderDataRecord(order.amount, order.playerUUID, order instanceof MarketOrder ? Order.Type.MARKET : Order.Type.LIMIT ,order.status);
+    public static @Nullable OrderDataRecord fromOrder(Order order, TradingPair pair){
+        UUID playerUUID = order.getPlayerUUID();
+        if (playerUUID == null) {
+            playerUUID = TEST_DUMMY_UUID; // Use a dummy UUID for testing purposes
+            // If you removed the TEST_DUMMY_UUID, you must return null here
+            // Do not create an instance of the record if the player is null since that means the order does not belong to any player
+            // and is a bot order!
+        }
+        return new OrderDataRecord(order.amount, playerUUID, order instanceof MarketOrder ? Order.Type.MARKET : Order.Type.LIMIT ,order.status, pair);
     }
 }
--- common/src/main/java/net/kroia/stockmarket/market/server/order/OrderHistory.java
+++ common/src/main/java/net/kroia/stockmarket/market/server/order/OrderHistory.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class OrderHistory /*implements ServerSaveableChunked*/ {
+public class OrderHistory {
     protected static StockMarketModBackend.Instances BACKEND_INSTANCES;
     public static void setBackend(StockMarketModBackend.Instances backend) {
         BACKEND_INSTANCES = backend;
@@ -22,7 +22,11 @@ public class OrderHistory /*implements ServerSaveableChunked*/ {
 
 
     private static final class OrderHistoryDataArchiveChunk extends DataArchiveChunk {
-
+        public static final class KEY
+        {
+            public static final String TRADING_PAIRS = "t";
+            public static final String CHRONOLOGICAL_ORDERS = "c";
+        }
         public final Map<TradingPair, List<OrderDataRecord>> marketsOrdersMap= new java.util.HashMap<>();
         public final List<OrderDataRecord> chronologicalOrderedOrderList = new java.util.ArrayList<>();
 
@@ -34,84 +38,152 @@ public class OrderHistory /*implements ServerSaveableChunked*/ {
         }
         @Override
         protected boolean save(CompoundTag dataTag) {
-            CompoundTag marketsOrdersTag = new CompoundTag();
+            ListTag tradingPairs = new ListTag();
+            Map<TradingPair, Integer> pairIDMap = new java.util.HashMap<>();
             for (Map.Entry<TradingPair, List<OrderDataRecord>> entry : marketsOrdersMap.entrySet()) {
                 CompoundTag marketTag = new CompoundTag();
                 entry.getKey().save(marketTag);
-                ListTag ordersList = new ListTag();
-                for (OrderDataRecord order : entry.getValue()) {
-                    CompoundTag orderTag = new CompoundTag();
-                    order.save(orderTag);
-                    ordersList.add(orderTag);
-                }
-                marketTag.put("orders", ordersList);
-                marketsOrdersTag.put(entry.getKey().toString(), marketTag);
+                pairIDMap.put(entry.getKey(), tradingPairs.size());
+                tradingPairs.add(marketTag);
+
             }
-            dataTag.put("marketsOrders", marketsOrdersTag);
+            dataTag.put(KEY.TRADING_PAIRS, tradingPairs);
 
             ListTag chronologicalOrdersList = new ListTag();
             for (OrderDataRecord order : chronologicalOrderedOrderList) {
                 CompoundTag orderTag = new CompoundTag();
                 order.save(orderTag);
+                orderTag.putInt(OrderDataRecord.KEY.TRADING_PAIR, pairIDMap.get(order.getTradingPair())); // Store the trading pair ID
                 chronologicalOrdersList.add(orderTag);
             }
-            dataTag.put("chronologicalOrders", chronologicalOrdersList);
+            dataTag.put(KEY.CHRONOLOGICAL_ORDERS, chronologicalOrdersList);
             return true;
         }
 
         @Override
         protected boolean load(CompoundTag dataTag) {
-            CompoundTag marketsOrdersTag = dataTag.getCompound("marketsOrders");
-            for (String marketKey : marketsOrdersTag.getAllKeys()) {
-                CompoundTag marketTag = marketsOrdersTag.getCompound(marketKey);
+
+            marketsOrdersMap.clear();
+            chronologicalOrderedOrderList.clear();
+
+            ListTag tradingPairs = dataTag.getList(KEY.TRADING_PAIRS, 10);
+            List<TradingPair> tradingPairsList = new ArrayList<>(tradingPairs.size());
+            for (int i = 0; i < tradingPairs.size(); i++) {
+                CompoundTag marketTag = tradingPairs.getCompound(i);
                 TradingPair pair = new TradingPair();
                 if (!pair.load(marketTag)) {
                     continue; // Skip if loading the trading pair fails
                 }
-                ListTag ordersList = marketTag.getList("orders", 10);
-                List<OrderDataRecord> orders = new java.util.ArrayList<>();
-                for (int i = 0; i < ordersList.size(); i++) {
-                    CompoundTag orderTag = ordersList.getCompound(i);
-                    OrderDataRecord order = new OrderDataRecord();
-                    order.load(orderTag);
-                    if (order != null) {
-                        orders.add(order);
-                    }
-                }
-                marketsOrdersMap.put(pair, orders);
+                marketsOrdersMap.put(pair, new ArrayList<>());
+                tradingPairsList.add(pair);
             }
 
-            ListTag chronologicalOrdersList = dataTag.getList("chronologicalOrders", 10);
-            chronologicalOrderedOrderList.clear();
-            for (int i = 0; i < chronologicalOrdersList.size(); i++) {
-                CompoundTag orderTag = chronologicalOrdersList.getCompound(i);
-                OrderDataRecord order = new OrderDataRecord();
-                if (order.load(orderTag)) {
-                    chronologicalOrderedOrderList.add(order);
+            ListTag ordersList = dataTag.getList(KEY.CHRONOLOGICAL_ORDERS, 10);
+            for(int i=0; i<ordersList.size(); i++)
+            {
+                CompoundTag orderTag = ordersList.getCompound(i);
+                int pairID = orderTag.getInt(OrderDataRecord.KEY.TRADING_PAIR);
+                if(pairID < 0 || pairID >= tradingPairsList.size())
+                {
+                    continue; // Invalid pair ID, skip this order
                 }
+                TradingPair pair = tradingPairsList.get(pairID);
+                OrderDataRecord order = OrderDataRecord.loadFromTag(orderTag, pair);
+                if(order == null)
+                {
+                    continue; // Skip if loading the order fails
+                }
+                marketsOrdersMap.get(pair).add(order);
+                chronologicalOrderedOrderList.add(order);
             }
             return true;
-
         }
-        // This class can be used to store order history data in a chunked manner if needed
-        // Currently, it is not implemented but can be extended in the future
     }
     private static final class OrderHistoryDataArchiveManager extends DataArchiveManager<OrderHistoryDataArchiveChunk>
     {
 
         private OrderHistoryDataArchiveChunk currentChunk;
+        private int learnedOrderCountSizeCheckEstimation = 0;
+
+
+        /*
+        // Performance tracking variables
+        private long savingTimesAccumul = 0;
+        private long loadingTimesAccumul = 0;
+        private long changeCheckTimesAccumul = 0;
+        private final List<Double> savingTimes = new ArrayList<>();
+        private final List<Double> changeCheckTimes = new ArrayList<>();
+        private final List<Double> loadingTimes = new ArrayList<>();
+        private boolean saveTimes = false; // Set this variable to true inside the debugger to trigger a dump to a csv file
+        */
+
 
         // Server constructor
         public OrderHistoryDataArchiveManager(Path archiveFolderPath) {
             super(archiveFolderPath, NBTFileParser.NbtFormat.COMPRESSED, OrderHistoryDataArchiveChunk::new);
+            setLogger(BACKEND_INSTANCES.LOGGER::error, BACKEND_INSTANCES.LOGGER::error, BACKEND_INSTANCES.LOGGER::debug, BACKEND_INSTANCES.LOGGER::warn);
             currentChunk = new OrderHistoryDataArchiveChunk();
+
+            /*
+            // Performance tracking
+            TickEvent.SERVER_POST.register(this::onClientTick);
+             */
         }
+
+
 
         public void hasChanged()
         {
-            if(getChunkSizeUtilisationPercentage(currentChunk) > 80) {
+            boolean needsNewChunk = false;
+            /*
+            // Performance tracking
+            long startTime = System.nanoTime();
+             */
+            if(learnedOrderCountSizeCheckEstimation != 0)
+            {
+                if(learnedOrderCountSizeCheckEstimation <= currentChunk.chronologicalOrderedOrderList.size())
+                {
+                    int currentOrderCount = currentChunk.chronologicalOrderedOrderList.size();
+                    float currentChunkSizePercentage = getChunkSizeUtilisationPercentage(currentChunk);
+                    learnedOrderCountSizeCheckEstimation = (int)(currentOrderCount / currentChunkSizePercentage * 80);
+                    needsNewChunk = currentChunkSizePercentage > 80;
+                }
+            }
+            else {
+                float currentChunkSizePercentage = getChunkSizeUtilisationPercentage(currentChunk);
+                if(currentChunkSizePercentage>20)
+                {
+                    int currentOrderCount = currentChunk.chronologicalOrderedOrderList.size();
+                    learnedOrderCountSizeCheckEstimation = (int)(currentOrderCount / currentChunkSizePercentage * 80);
+                }
+                needsNewChunk = currentChunkSizePercentage > 80;
+            }
+            /*
+            // Performance tracking
+            changeCheckTimesAccumul += System.nanoTime() - startTime;
+            */
+
+
+            if(needsNewChunk)
+            {
                 long endTime = currentChunk.updateEndTime();
+                /*
+                // Performance tracking
+                long start = System.nanoTime();
+                 */
                 saveChunk(currentChunk);
+
+                /*
+                // Performance tracking
+                long end = System.nanoTime();
+                savingTimesAccumul += (end - start);
+
+
+                // dummy load for performance tracking
+                load();
+                 */
+
+                learnedOrderCountSizeCheckEstimation = currentChunk.chronologicalOrderedOrderList.size();
                 currentChunk = new OrderHistoryDataArchiveChunk(endTime+1);
             }
         }
@@ -127,6 +199,11 @@ public class OrderHistory /*implements ServerSaveableChunked*/ {
         }
         public boolean load()
         {
+            /*
+            // Performance tracking
+            long start = System.nanoTime();
+             */
+
             // load the most recent chunk
             List<DataArchiveChunk.TimeInterval> chunkIntervals = getStoredIntervals();
             if(!chunkIntervals.isEmpty())
@@ -135,19 +212,83 @@ public class OrderHistory /*implements ServerSaveableChunked*/ {
                 if(list != null && !list.isEmpty())
                 {
                     currentChunk = list.get(0);
+                    /*
+                    // Performance tracking
+                    long end = System.nanoTime();
+                    loadingTimesAccumul += (end - start);
+                     */
                     return true;
                 }
             }
             return false;
         }
+
+        /*
+        // Performance tracking
+        private void onClientTick(MinecraftServer server) {
+            if(savingTimesAccumul > 0) {
+                savingTimes.add(savingTimesAccumul / 1_000_000_000.0); // Convert to seconds
+                savingTimesAccumul = 0;
+            }
+
+
+            if(loadingTimesAccumul > 0) {
+                loadingTimes.add(loadingTimesAccumul / 1_000_000_000.0); // Convert to seconds
+                loadingTimesAccumul = 0;
+            }
+
+
+            // if(changeCheckTimesAccumul > 0) {
+            //     changeCheckTimes.add(changeCheckTimesAccumul / 1_000_000.0); // Convert to milliseconds
+            //     changeCheckTimesAccumul = 0;
+            // }
+
+            // Put a breakpoint before the if statement if you want to save the performance data to a CSV file.
+            // Set the variable to true in the debugger.
+            // The variable gets set to false after the data is saved.
+            if(saveTimes)
+            {
+                savePerformanceTimes();
+            }
+        }
+
+        private void savePerformanceTimes()
+        {
+            saveTimes = false; // Reset the flag after saving times
+            // create csv file with saving and loading times
+            StringBuilder csvBuilder = new StringBuilder();
+            csvBuilder.append("Check Times (ms); Saving Times (seconds); Loading Times (seconds)\n");
+            int maxSize = Math.max(Math.max(savingTimes.size(), loadingTimes.size()), changeCheckTimes.size());
+            for(int i = 0; i < maxSize; i++)
+            {
+                if(i < changeCheckTimes.size())
+                {
+                    csvBuilder.append(changeCheckTimes.get(i));
+                }
+                csvBuilder.append(";");
+                if(i < savingTimes.size())
+                {
+                    csvBuilder.append(savingTimes.get(i));
+                }
+                csvBuilder.append(";");
+                if(i < loadingTimes.size())
+                {
+                    csvBuilder.append(loadingTimes.get(i));
+                }
+                csvBuilder.append("\n");
+            }
+            Path csvPath = getArchiveFolderPath().resolve("../saving_loading_times.csv");
+            try {
+                java.nio.file.Files.writeString(csvPath, csvBuilder.toString());
+            } catch (java.io.IOException e) {
+                BACKEND_INSTANCES.LOGGER.error("Failed to write saving/loading times to CSV file: " + csvPath, e);
+            }
+            savingTimes.clear();
+            loadingTimes.clear();
+            changeCheckTimes.clear();
+        }
+        */
     }
-
-
-
-    /*static final int ORDER_CONTEXT = 50;
-
-    protected Map<TradingPair, Order[]> orderData;
-    protected Order[] mostRecentOrders;*/
 
     private final OrderHistoryDataArchiveManager orderHistoryDataArchiveManager;
     private final OrderHistoryDataArchiveChunk clientChunkData;
@@ -157,21 +298,22 @@ public class OrderHistory /*implements ServerSaveableChunked*/ {
     public OrderHistory(Path orderHistoryFolder){
         orderHistoryDataArchiveManager = new OrderHistoryDataArchiveManager(orderHistoryFolder);
         clientChunkData = null;
-        //orderData = new HashMap<>();
-        //mostRecentOrders = new Order[ORDER_CONTEXT];
     }
 
     // Client constructor
     public OrderHistory(){
         orderHistoryDataArchiveManager = null;
         clientChunkData = new OrderHistoryDataArchiveChunk();
-        //orderData = new HashMap<>();
-        //mostRecentOrders = new Order[ORDER_CONTEXT];
     }
 
     public boolean putOrder(TradingPair pair, Order order){
 
         /*
+        IMPORTANT!!
+        If you enable this code section to remove the bot orders from being saved,
+        you must also remove the TEST_DUMMY_UUID from the OrderDataRecord class because that was just a workaround for testing purposes.
+
+
         if(order.isBot())
         {
             // Don't add bot orders
@@ -182,19 +324,24 @@ public class OrderHistory /*implements ServerSaveableChunked*/ {
 
         if(orderHistoryDataArchiveManager != null)
         {
-            orderHistoryDataArchiveManager.currentChunk.chronologicalOrderedOrderList.add(OrderDataRecord.fromOrder(order));
-            List<OrderDataRecord> orders = orderHistoryDataArchiveManager.currentChunk.marketsOrdersMap.computeIfAbsent(pair, k -> new java.util.ArrayList<>());
-            orders.add(OrderDataRecord.fromOrder(order));
-            BACKEND_INSTANCES.LOGGER.info("[OrderHistory::putOrder()]: Order added to history, OrderID: " + order.getOrderID());
-            orderHistoryDataArchiveManager.hasChanged();
+            OrderDataRecord orderDataRecord = OrderDataRecord.fromOrder(order, pair);
+            if(orderDataRecord != null) {
+                orderHistoryDataArchiveManager.currentChunk.chronologicalOrderedOrderList.add(orderDataRecord);
+                List<OrderDataRecord> orders = orderHistoryDataArchiveManager.currentChunk.marketsOrdersMap.computeIfAbsent(pair, k -> new java.util.ArrayList<>());
+                orders.add(orderDataRecord);
+                orderHistoryDataArchiveManager.hasChanged();
+            }
             return true;
         }
 
         if(clientChunkData != null)
         {
-            clientChunkData.chronologicalOrderedOrderList.add(OrderDataRecord.fromOrder(order));
-            List<OrderDataRecord> orders = clientChunkData.marketsOrdersMap.computeIfAbsent(pair, k -> new java.util.ArrayList<>());
-            orders.add(OrderDataRecord.fromOrder(order));
+            OrderDataRecord orderDataRecord = OrderDataRecord.fromOrder(order, pair);
+            if(orderDataRecord != null) {
+                clientChunkData.chronologicalOrderedOrderList.add(orderDataRecord);
+                List<OrderDataRecord> orders = clientChunkData.marketsOrdersMap.computeIfAbsent(pair, k -> new java.util.ArrayList<>());
+                orders.add(orderDataRecord);
+            }
             return true;
         }
         return false;
@@ -211,20 +358,6 @@ public class OrderHistory /*implements ServerSaveableChunked*/ {
         }
         return new ArrayList<>();
     }
-
-    public void logOrder(Order order, Order[] orders){
-        for(int i = 0; i < orders.length; ++i){
-            if(orders[i] == null){
-                orders[i] = order;
-                return;
-            }
-        }
-        //History is full, drop off the oldest order
-        System.arraycopy(orders, 1, orders, 0, orders.length-1);
-        orders[orders.length-1] = order;
-    }
-
-
 
     public boolean save()
     {
@@ -250,76 +383,4 @@ public class OrderHistory /*implements ServerSaveableChunked*/ {
         }
         return false;
     }
-
-    /*@Override
-    public boolean save(Map<String, ListTag> listTags) {
-        boolean success = true;
-        ListTag historyTag = new ListTag();
-        CompoundTag tag = new CompoundTag();
-        success = writeMarketOrderHistory(mostRecentOrders, tag);
-        historyTag.add(tag);
-        orderData.forEach((key, value) -> {
-            CompoundTag orderTag = new CompoundTag();
-            key.save(orderTag);
-            writeMarketOrderHistory(value, orderTag);
-            historyTag.add(orderTag);
-        });
-
-        listTags.put("orderHistory", historyTag);
-
-        return success;
-    }
-
-    public boolean writeMarketOrderHistory(Order[] orders, CompoundTag tag){
-        boolean success = true;
-        int i = 0;
-        ListTag list = new ListTag();
-        while(orders[i] != null){
-            CompoundTag orderTag = new CompoundTag();
-            success &= orders[i].save(orderTag);
-            orderTag.putString("type", orders[i].getType().toString());
-            list.add(orderTag);
-        }
-        tag.put("orders", list);
-        return success;
-    }
-
-    public boolean readMarketOrderHistory(Order[] orders, CompoundTag tag){
-        boolean success = true;
-        ListTag list = tag.getList("orders", 10);
-        for(int i = 0; i < Math.min(ORDER_CONTEXT, list.size()); ++i){
-            CompoundTag orderTag = list.getCompound(i);
-            Order order;
-            if(orderTag.getString("type").equals("limit")){
-                order = LimitOrder.loadFromTag(orderTag);
-            }
-            else{
-                order = MarketOrder.loadFromTag(tag);
-            }
-            orders[i] = order;
-
-        }
-        return success;
-    }
-
-    @Override
-    public boolean load(Map<String, ListTag> listTags) {
-        if(!listTags.containsKey("orderHistory")){
-            return false;
-        }
-        ListTag historyTag = listTags.get("orderHistory");
-        for(int i = 0; i < historyTag.size(); ++i){
-            CompoundTag tag = historyTag.getCompound(i);
-            TradingPair pair = new TradingPair();
-            Order[] orders = new Order[ORDER_CONTEXT];
-            readMarketOrderHistory(orders, tag);
-            if(!pair.load(tag)){
-                mostRecentOrders = orders;
-            }
-            else {
-                orderData.put(pair, orders);
-            }
-        }
-        return true;
-    }*/
 }
