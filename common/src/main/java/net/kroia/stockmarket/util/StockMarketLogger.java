f334909c11556cefbc69abcacbb2f60b87e8fb8b
Alex <alexkrieg@gmx.ch>
2026-05-18 17:47:15 +0200

    ! fix NPE in StockMarketLogger when INSTANCES is null before backend init

--- common/src/main/java/net/kroia/stockmarket/util/StockMarketLogger.java
+++ common/src/main/java/net/kroia/stockmarket/util/StockMarketLogger.java
@@ -16,35 +16,35 @@ public class StockMarketLogger {
 
     public void info(String message) {
         boolean enabled = true;
-        if(INSTANCES.SERVER_SETTINGS != null)
+        if(INSTANCES != null && INSTANCES.SERVER_SETTINGS != null)
             enabled = INSTANCES.SERVER_SETTINGS.UTILITIES.LOGGING_ENABLE_INFO.get();
         if(enabled)
             LOGGER.info(message);
     }
     public void error(String message) {
         boolean enabled = true;
-        if(INSTANCES.SERVER_SETTINGS != null)
+        if(INSTANCES != null && INSTANCES.SERVER_SETTINGS != null)
             enabled = INSTANCES.SERVER_SETTINGS.UTILITIES.LOGGING_ENABLE_ERROR.get();
         if(enabled)
             LOGGER.error(message);
     }
     public void error(String message, Throwable throwable) {
         boolean enabled = true;
-        if(INSTANCES.SERVER_SETTINGS != null)
+        if(INSTANCES != null && INSTANCES.SERVER_SETTINGS != null)
             enabled = INSTANCES.SERVER_SETTINGS.UTILITIES.LOGGING_ENABLE_ERROR.get();
         if(enabled)
             LOGGER.error(message, throwable);
     }
     public void warn(String message) {
         boolean enabled = true;
-        if(INSTANCES.SERVER_SETTINGS != null)
+        if(INSTANCES != null && INSTANCES.SERVER_SETTINGS != null)
             enabled = INSTANCES.SERVER_SETTINGS.UTILITIES.LOGGING_ENABLE_WARNING.get();
         if(enabled)
             LOGGER.warn(message);
     }
     public void debug(String message) {
        boolean enabled = true;
-       if(INSTANCES.SERVER_SETTINGS != null)
+       if(INSTANCES != null && INSTANCES.SERVER_SETTINGS != null)
            enabled = INSTANCES.SERVER_SETTINGS.UTILITIES.LOGGING_ENABLE_DEBUG.get();
        if(enabled)
             LOGGER.debug(message);
